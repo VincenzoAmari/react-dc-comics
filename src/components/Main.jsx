d0df252b963b6f006af93d226948355b13cafc7b
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-18 18:37:26 +0100

    main provvisorio

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,26 @@
 const Main = () => {
     return (
-    <h1>ciao</h1>
+        <div className="bg-primary text-white py-4 text-center">
+        <div className="container">
+          <div className="row">
+            <div className="col-md">
+                <p> DIGITAL COMICS</p>
+                </div>
+            <div className="col-md">
+                <p> DC MERCHANDISE</p>
+                </div>
+            <div className="col-md">
+                <p> SUBSCRIPTION</p>
+                </div>
+            <div className="col-md">
+                <p> COMIC SHOP LOCATOR</p>
+                </div>
+            <div className="col-md">
+                <p> DC POWER VISA</p>
+                </div>
+          </div>
+        </div>
+      </div>
 )
 }
 export default Main
