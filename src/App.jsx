d220bf2313e62a91731a09f2bf6933862d1ff754
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-19 17:13:47 +0100

    link della navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,26 @@ import Main from "./components/Main"
 import Footer from "./components/Footer"
 
 function App() {
+  const navbarLinks = [
+    { label: 'CHARACTERS', url: '#' },
+    { label: 'COMICS', url: '#' },
+    { label: 'MOVIES', url: '#' },
+    { label: 'TV', url: '#' },
+    { label: 'GAMES', url: '#' },
+    { label: 'COLLECTIBLES', url: '#' },
+    { label: 'VIDEOS', url: '#' },
+    { label: 'FANS', url: '#' },
+    { label: 'NEWS', url: '#' },
+    { label: 'SHOP', url: '#' },
+  ];
   
   return (
-      <>
-        <Navbar />
+      <div className="App">
+        <Navbar links={navbarLinks} />
         <HeroSection/>
         <Main />
         <Footer />
-      </>
+      </div>
   )
 }
 
