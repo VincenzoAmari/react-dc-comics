bb6530ab230f45fc07da88d937a8529cadb587e2
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-19 17:19:21 +0100

    array alla navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,17 @@
 
-function Navbar() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-        
-                                  {/* Logo */}
-          <a className="navbar-brand" href="#">
-            <img src="src/assets/img/dc-logo.png" alt="DC Logo" width="50" />
-          </a>
-          
-       
-       
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item"><a className="nav-link" href="#">Characters</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">Comics</a></li> 
-              <li className="nav-item"><a className="nav-link" href="#">Movies</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">TV</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">Games</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">Collectibles</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">Videos</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">Fans</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">News</a></li>
-              <li className="nav-item"><a className="nav-link" href="#">Shop</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+function Navbar({ links }) {
+  return (
+    <header className="bg-light p-3">
+      <div className="d-flex justify-content-between align-items-center">
+        <img src="src/assets/img/dc-logo.png" alt="DC Logo" className="img-fluid" style={{height: '50px'}} />
+        <nav>
+          {links.map((link, index) => (
+            <a key={index} href={link.url} className="mx-2 text-decoration-none text-dark font-weight-bold">{link.label}</a>
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
+}
   
   export default Navbar;
