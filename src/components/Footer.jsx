4a3659447372c9df8eefcb8746bcaf69767cf455
VincenzoAmari <vincenzo.am99@gmail.com>
2025-02-19 16:53:07 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,67 +3,70 @@
 
 function Footer() {
     return (
-        <footer className="bg-dark text-white py-4">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-3">
-                        <h5>DC COMICS</h5>
-                        <ul className="list-unstyled">
-                            <li>Characters</li>
-                            <li>Comics</li>
-                            <li>Movies</li>
-                            <li>TV</li>
-                            <li>Games</li>
-                            <li>Videos</li>
-                            <li>News</li>
-                        </ul>
-                    </div>
-                    <div className="col-md-3">
-                        <h5>DC</h5>
-                        <ul className="list-unstyled">
-                            <li>Terms Of Use</li>
-                            <li>Privacy policy(New)</li>
-                            <li>Ad Choices</li>
-                            <li>Advertising</li>
-                            <li>Jobs</li>
-                            <li>Subscriptions</li>
-                            <li>Talent Workshops</li>
-                            <li>CPSC Certificates</li>
-                            <li>Ratings</li>
-                        </ul>
-                    </div>
-                    <div className="col-md-3">
-                        <h5>SITES</h5>
-                        <ul className="list-unstyled">
-                            <li>DC</li>
-                            <li>MAD Magazine</li>
-                            <li>DC Kids</li>
-                            <li>DC Universe</li>
-                            <li>DC Power Visa</li>
-                        </ul>
-                    </div>
-                    <div className="col-md-3">
-                        <h5>SHOP</h5>
-                        <ul className="list-unstyled">
-                            <li>Shop DC</li>
-                            <li>Shop DC Collectibles</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="row mt-4">
-                    <div className="col-12 d-flex justify-content-between align-items-center">
-                        <button className="btn btn-outline-primary">SIGN-UP NOW!</button>
-                        <div className="follow-us text-primary d-flex align-items-center">
-                            <span>FOLLOW US</span>
-                            <div className="social-icons d-flex">
-                                <a href="#"><img src="src/assets/img/footer-facebook.png" alt="Facebook" className="img-fluid mx-2" style={{height: '20px'}} /></a>
-                                <a href="#"><img src="src/assets/img/footer-twitter.png" alt="Twitter" className="img-fluid mx-2" style={{height: '20px'}} /></a>
-                                <a href="#"><img src="src/assets/img/footer-youtube.png" alt="YouTube" className="img-fluid mx-2" style={{height: '20px'}} /></a>
-                                <a href="#"><img src="src/assets/img/footer-pinterest.png" alt="Pinterest" className="img-fluid mx-2" style={{height: '20px'}} /></a>
-                                <a href="#"><img src="src/assets/img/footer-periscope.png" alt="Periscope" className="img-fluid mx-2" style={{height: '20px'}} /></a>
-                            </div>
+        <footer>
+            <div className="container-fluid bg-dark text-white" style={{backgroundImage: 'url(src/assets/img/footer-bg.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', paddingBottom: '100px'}}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-2 py-5">
+                            <h5>DC COMICS</h5>
+                            <ul className="list-unstyled">
+                                <li>Characters</li>
+                                <li>Comics</li>
+                                <li>Movies</li>
+                                <li>TV</li>
+                                <li>Games</li>
+                                <li>Videos</li>
+                                <li>News</li>
+                            </ul>
+                            <h5 className="mt-4">SHOP</h5>
+                            <ul className="list-unstyled">
+                                <li>Shop DC</li>
+                                <li>Shop DC Collectibles</li>
+                            </ul>
+                        </div>
+                        <div className="col-2 py-5">
+                            <h5>DC</h5>
+                            <ul className="list-unstyled">
+                                <li>Terms Of Use</li>
+                                <li>Privacy policy(New)</li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                            </ul>
+                        </div>
+                        <div className="col-2 py-5">
+                            <h5>SITES</h5>
+                            <ul className="list-unstyled">
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
                         </div>
                     </div>
+                </div>
+                <div className="text-center position-absolute" style={{right: '5%', top: '50%', transform: 'translateY(-50%)'}}>
+                <img src="src/assets/img/dc-logo-bg.png" alt="DC Centered Image" className="img-fluid" style={{maxWidth: '500px'}} />
+                </div>
+            </div>
+            <div className="container-fluid bg-dark text-white py-3" style={{position: 'relative', zIndex: 1}}>
+                <div className="container d-flex justify-content-between align-items-center">
+                <button className="btn btn-outline-primary">SIGN-UP NOW!</button>
+                <div className="follow-us text-primary d-flex align-items-center">
+                    <span>FOLLOW US</span>
+                    <div className="social-icons d-flex ml-2">
+                    <a href="#"><img src="src/assets/img/footer-facebook.png" alt="Facebook" className="img-fluid mx-1" style={{height: '20px'}} /></a>
+                    <a href="#"><img src="src/assets/img/footer-twitter.png" alt="Twitter" className="img-fluid mx-1" style={{height: '20px'}} /></a>
+                    <a href="#"><img src="src/assets/img/footer-youtube.png" alt="YouTube" className="img-fluid mx-1" style={{height: '20px'}} /></a>
+                    <a href="#"><img src="src/assets/img/footer-pinterest.png" alt="Pinterest" className="img-fluid mx-1" style={{height: '20px'}} /></a>
+                    <a href="#"><img src="src/assets/img/footer-periscope.png" alt="Instagram" className="img-fluid mx-1" style={{height: '20px'}} /></a>
+                    </div>
+                </div>
                 </div>
             </div>
         </footer>
@@ -71,3 +74,4 @@ function Footer() {
     }
     
     export default Footer;
+    
